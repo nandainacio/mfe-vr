94ef8e1fa12ad59718b8a8424a73ce2896b2d677
Fernanda Inacio <fp_inacio@hotmail.com>
2026-05-18 20:05:56 -0300

    inclusão header e ajuste footer

--- apps/header/src/app/app.tsx
+++ apps/header/src/app/app.tsx
@@ -1,11 +1,19 @@
-// Uncomment this line to use CSS modules
-// import styles from './app.module.css';
-import NxWelcome from './nx-welcome';
+import vrLogo from '../assets/logo_vr_branco.png';
+import imgCompras from '../assets/btn_compras.png';
+import styles from './app.module.css';
 
 export function App() {
   return (
-    <div>
-      <NxWelcome title="header" />
+    <div className={styles.container}>
+      <img className={styles.logoBranco} src={vrLogo} alt="VR Logo" />
+      <button className={styles.botaoCarrinho} onClick={() => alert('Carrinho clicado!')}>
+        <img
+          src={imgCompras}
+          className={styles.imgCompras}
+          alt="Ícone do Carrinho"
+        />
+        <span className={styles.contador}>0</span>
+      </button>
     </div>
   );
 }
